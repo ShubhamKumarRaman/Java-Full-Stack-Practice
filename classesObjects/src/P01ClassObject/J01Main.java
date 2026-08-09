bb344fb982785aa6abc5b1815a49ff9f0743b3a3
Shubham Kumar <shubham620409@gmail.com>
2026-08-09 17:40:15 +0530

    Update J01Main.java

--- classesObjects/src/P01ClassObject/J01Main.java
+++ classesObjects/src/P01ClassObject/J01Main.java
@@ -1,7 +1,20 @@
 package P01ClassObject;
 
-public class J01Main    {
-    public static void main(String args[]){
-        
+class Car {
+    String color;
+    String model;
+
+    void display() {
+        System.out.println("Color: " + this.color);
+        System.out.println("Model: " + this.model);
+    }
+}
+
+public class J01Main {
+    public static void main(String args[]) {
+        Car c = new Car();
+        c.color = "red";
+        c.model = "BMW";
+        c.display();
     }
 }
