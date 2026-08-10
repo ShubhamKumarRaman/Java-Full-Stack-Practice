20273eb465a5bb466942ca66ef33d3e666aa5c55
Shubham Kumar <shubham620409@gmail.com>
2026-08-10 09:21:46 +0530

    Update J06Super.java

--- classesObjects/src/P03Inheritance/J06Super.java
+++ classesObjects/src/P03Inheritance/J06Super.java
@@ -1,12 +1,16 @@
 package P03Inheritance;
 
 class Parent2 {
+    int value = 10;
+
     void display() {
         System.out.println("Display Parent");
     }
 }
 
 class Child2 extends Parent2 {
+    int value = 20;
+
     void display() {
         System.out.println("Display Child");
     }
@@ -16,11 +20,20 @@ class Child2 extends Parent2 {
         super.display();
         // display();
     }
+
+    void showValue() {
+        System.out.println("Parent value: " + super.value);
+        System.out.println("Child value: " + value);
+    }
 }
 
 public class J06Super {
     public static void main(String args[]) {
         Child2 obj = new Child2();
         obj.show();
+        obj.showValue();
+
+        System.out.println(obj.value);
+//        System.out.println(obj.super.value);
     }
 }
